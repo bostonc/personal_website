4121ff5cc1ee3150973176504af1f207561999a2
bostonc <bostonchris322@gmail.com>
2021-03-23 15:37:24 -0400

    Initial content commit

---
+++ src/App.js
@@ -0,0 +1,99 @@
+/* eslint-disable react/prop-types */
+import { React, Component } from 'react';
+import logo from './logo.svg';
+import resume from './Chris_Boston_Full_Stack_Resume.pdf';
+import './App.css';
+
+process.title = "myReactApp";
+
+class App extends Component {
+  componentDidMount() {
+    document.title = "chrisboston.info";
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="ContentBox">
+          <header className="App-header">
+            <h1>Chris Boston</h1>
+            <h2>Software Engineer</h2>
+          </header>
+          <NavBar/>
+          <ContactInfo/>
+          <Attribution/>
+        </div>  
+      </div>
+    );
+  }
+}
+
+class ContactInfo extends Component {
+  render() {
+    return (
+      <div className="ContactInfo">
+        <h4 className="LetsConnect">{"Let's connect"}</h4>
+        <p className="ContactDetails">
+          (240) 630-4185
+          <br/>
+          bostonc@umich.edu  
+        </p>
+      </div>
+    );
+  }
+}
+
+class Attribution extends Component {
+  render() {
+    return (
+      <div className="AttributionContainer">
+        <span className="AttributionText">Powered by React.js</span>
+        <span className="LogoBox">
+          <img src={logo} className="App-logo" alt="logo" />
+        </span>
+      </div>
+    );
+  }
+}
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div className="NavBar">
+        <NavButton text="Github" link="https://github.com/bostonc"/>
+        <NavButton text="LinkedIn" link="https://www.linkedin.com/in/bostonchrisb/"/>
+        <NavButton text="AngelList" link="https://angel.co/bostonchrisb"/>
+        <NavButton text="Twitter" link="https://twitter.com/bostonc322"/>
+        <NavButton text="Résumé" link={ resume }/>
+      </div>
+    );
+  }
+}
+
+class NavButton extends Component {
+  render() {
+    return (
+      <span>
+        <a 
+          href={this.props.link}
+          rel="noreferrer"
+          target="_blank"
+        >
+          <button className="NavButton">
+            <svg viewBox="0 0 100 60" 
+              preserveAspectRatio="none"
+              className="border"
+            >
+              <polyline points="99,1 99,59 1,59 1,1 99,1" 
+                vectorEffect="non-scaling-stroke"
+                className="hl-line"/>
+            </svg>
+            <span className="ButtonText">{this.props.text}</span>
+          </button>
+        </a>
+      </span>
+    );
+  }
+}
+
+export default App;
+
